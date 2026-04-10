d29dd9363f99f8103c0e2606535fe62184edae2a
Jjrwn <jerwinmacasinag0328@gmail.com>
2026-04-10 16:46:55 +0800

    change url

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,7 +1,6 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
-import ContextProvider from "../context/ContextProvider";
 
 export default function ProtectedRoute({ children, role }) {
   const { user } = useContext(AuthContext);
